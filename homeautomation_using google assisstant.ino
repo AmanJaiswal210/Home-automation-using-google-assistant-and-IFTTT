4f433727b071e23b339a105cc60d1e155e40183e
Aman jaiswal <48851538+AmanJaiswal210@users.noreply.github.com>
2019-04-17 03:57:33 -0700

    Add files via upload

---
+++ homeautomation_using google assisstant.ino
@@ -0,0 +1,127 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WIFI_SSID "Tenda_7F50C8"
+#define WIFI_PASS "Vansh024@"
+
+#define MQTT_SERV "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_NAME "amanjaiswal210"
+#define MQTT_PASS "bdc10d77266343b195209da875d302c1"
+
+int led = D7;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);
+
+Adafruit_MQTT_Subscribe onoff = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f/ONOF");
+Adafruit_MQTT_Publish LedStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/LedStatus");
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
+  //Connect to WiFi
+  Serial.print("\n\nConnecting Wifi>");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(">");
+    delay(50);
+  }
+
+  Serial.println("OK!");
+
+  //Subscribe to the onoff topic
+  mqtt.subscribe(&onoff);
+
+  pinMode(led, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(led, LOW);
+
+}
+
+void loop()
+{
+  //Connect/Reconnect to MQTT
+  MQTT_connect();
+
+  //Read from our subscription queue until we run out, or
+  //wait up to 5 seconds for subscription to update
+  Adafruit_MQTT_Subscribe * subscription;
+  while ((subscription = mqtt.readSubscription(5000)))
+  {
+    //If we're in here, a subscription updated...
+    if (subscription == &onoff)
+    {
+      //Print the new value to the serial monitor
+      Serial.print("onoff: ");
+      Serial.println((char*) onoff.lastread);
+
+      //If the new value is  "ON", turn the light on.
+      //Otherwise, turn it off.
+      if (!strcmp((char*) onoff.lastread, "ON"))
+      {
+        //active low logic
+        digitalWrite(led, HIGH);
+        LedStatus.publish("ON");
+      }
+      else if (!strcmp((char*) onoff.lastread, "OFF"))
+      {
+        digitalWrite(led, LOW);
+        LedStatus.publish("OFF");
+
+      }
+      else
+      {
+        LedStatus.publish("ERROR");
+      }
+    }
+    else
+    {
+      //LedStatus.publish("ERROR");
+    }
+  }
+  //  if (!mqtt.ping())
+  //  {
+  //    mqtt.disconnect();
+  //  }
+}
+
+
+void MQTT_connect()
+{
+
+  //  // Stop if already connected
+  if (mqtt.connected() && mqtt.ping())
+  {
+    //    mqtt.disconnect();
+    return;
+  }
+
+  int8_t ret;
+
+  mqtt.disconnect();
+
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) // connect will return 0 for connected
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0)
+    {
+      ESP.reset();
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
